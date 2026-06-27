60a71719a23772ad4aa825273954f706cee2cbbf
agil999 <115994499+agil999@users.noreply.github.com>
2026-06-27 20:57:15 +0700

    Fix Vercel and Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    minify: 'terser',
   },
   server: {
     port: 3000,
     host: true,
   },
+  root: './src',
 })
